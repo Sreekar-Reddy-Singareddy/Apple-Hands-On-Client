538a8463bffb7c7f0b24dfa0c1d3d18c93e5010f
Shree9119 <sreekarreddy430@gmail.com>
2019-01-07 15:34:46 +0530

    Created backend project
    This project contains the code written in Java. Uses servlet as Tomcat. Developed in Netbeans IDE.

---
+++ SR_HandsOn/SR_HandsOn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SR_HandsOn
+//
+//  Created by Bros on 28/12/18.
+//  Copyright © 2018 Singareddy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SR_HandsOn/SR_HandsOn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SR_HandsOn
+//
+//  Created by Bros on 28/12/18.
+//  Copyright © 2018 Singareddy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
